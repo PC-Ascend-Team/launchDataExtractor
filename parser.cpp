5e19da8b065902c4f693045e7507e355224a4d38
Darthpbal <ronquillo.paul@gmail.com>
2016-03-12 17:55:19 -0700

    sync test version with master version code

--- parser.cpp
+++ parser.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 char delimiter = ',';
 
+//function prototype to settle scope access.
 string extractFromLine(string sourceLine, int seriesSize, int posSeries[]);
 
 int main(){
-	string name; int colArrSize = 0;
+	string name;
+	int colArrSize = 0;
 
 	//user interface
 	cout << "What data are you extracting? (oneWord): ";
@@ -18,12 +20,13 @@ int main(){
 	name = name + ".txt";
 	cout << "How many columns?: ";
 	cin >> colArrSize;
-	int colArray[colArrSize];
-	string data[colArrSize];
+	int colArray[colArrSize];//makes the array of columns to extract arbitrarily sized
 	cout << "Enter each position number \n(starting from 0) \nfollowed by the enter key: \n";
+	//collect positions to extract
 	for (int i = 0; i < colArrSize; i++) {
 		cin >> colArray[i];
 	}
+
 
 	//open and check input/output files
 	ifstream inputFile("rawData");
@@ -37,25 +40,30 @@ int main(){
 		exit(-1);
 	}
 
+
 	//process file line by line
 	string line;
+	string processedLine;
 	while(!inputFile.eof()){
 		getline(inputFile, line);
-		string processedLine;
 		processedLine = extractFromLine(line, colArrSize, colArray);
-		outputFile << processedLine;
-		line.clear();
+		outputFile << processedLine; // write to output file
+		line.clear();//empty line to clean up for next run
 	}
 
+	//cleanup files and terminal
 	inputFile.close();
 	outputFile.close();
 	cout << endl;
+	return 0;
 }
 
 
-
-
-
+/*
+	sourceLine = the line to be sorted
+	seriesSize = the number of columns to extract
+	posSeries = the array of ints collected from the user. Represents the actual positions to extract.
+*/
 string extractFromLine(string sourceLine, int seriesSize, int posSeries[]){
 	string outPutString = "";
 	//perform extraction on line
@@ -63,53 +71,18 @@ string extractFromLine(string sourceLine, int seriesSize, int posSeries[]){
 	for(int linePos = 0; linePos < sourceLine.size(); linePos++){
 		if(sourceLine[linePos] == ',') {
 			position++;
-		}
-		else{
+		}else{
 			for (int colToExtract = 0; colToExtract < seriesSize; colToExtract++) {
 				//if this position matches a position to extract
 				if(position == posSeries[colToExtract]){
 					//print the current line character
 					outPutString += sourceLine[linePos];
-					//if we're about to change position, add delimiter
+					//if we're about to change position, add delimiter. This will leave a comma at the end!
 					if(sourceLine[linePos + 1] == ',') outPutString += delimiter;
 				}
 			}
 		}
 	}
-	// int test = ;
-	// cout << outPutString[outPutString.length() - 1];
-	// outPutString.replace((outPutString.length() - 1), 1, "\n");
-	// outPutString[outPutString.length() - 1] = "\n";
-
-	// cout << outPutString.substr(0, (outPutString.length() - 1)) << endl;
-	// // replace the last comma with a newline
-	// outPutString += "\n";
+	// return the string but with last comma replace with newline char
 	return outPutString.substr(0, (outPutString.length() - 1)) + "\n";
 }
-
-/*
-
-eg3. $GPRMC,220516,A,5133.82,N,00042.24,W,173.8,231.8,130694,004.2,W*70
-			 1    2    3    4    5     6    7    8      9     10  11 12
-
-	 1   220516     Time Stamp
-	 2   A          validity - A-ok, V-invalid
-	 3   5133.82    current Latitude
-	 4   N          North/South
-	 5   00042.24   current Longitude
-	 6   W          East/West
-	 7   173.8      Speed in knots
-	 8   231.8      True course
-	 9   130694     Date Stamp
-	 10  004.2      Variation
-	 11  W          East/West
-	 12  *70        checksum
-
-
-
-	 In this sketch, we call GPS.read() within a once-a-millisecond timer (this is the same timer that runs the millis() command). Then in the main loop we can ask if a new chunk of data has been received by calling GPS.newNMEAreceived(), if this returns true then we can ask the library to parse that data with GPS.parse(GPS.lastNMEA()).
-
-	We do have to keep querying and parsing in the main loop - its not possible to do this in an interrupt because then we'd be dropping GPS data by accident.
-	Once data is parsed, we can just ask for data from the library like GPS.day, GPS.month and GPS.year for the current date. GPS.fix will be 1 if there is a fix, 0 if there is none. If we have a fix then we can ask for GPS.latitude, GPS.longitude, GPS.speed (in knots, not mph or k/hr!), GPS.angle, GPS.altitude (in centimeters) and GPS.satellites (number of satellites)
-
-*/
